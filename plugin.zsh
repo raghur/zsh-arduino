de7dc73a1414bdc4e289213746cf9dab9ffb2d76
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2020-09-30 13:12:13 +0530

    check pre-reqs in plugin.zsh

--- plugin.zsh
+++ plugin.zsh
@@ -1,8 +1,17 @@
 #! /bin/bash
-autoload ar_configure
-autoload ar_build
-autoload ar_upload
-autoload ar_serial
-autoload ar_env
+JQ=jq
+ACLI=arduino-cli
+err=$(command -v $JQ &> /dev/null || printf "%s" $JQ)
+err+=$(command -v $ACLI &> /dev/null || printf ", %s" $ACLI)
+if [[ -z "$err" ]]; then
+    autoload ar_configure
+    autoload ar_build
+    autoload ar_upload
+    autoload ar_serial
+    autoload ar_env
+    # echo "arduino plugin registered"
+else
+    echo "zsh-arduino: Commands not found on path: $err; Plugin not registered"
+fi
 
 
