faa6c97fb029e63ffc7d227ae24423f1844c1132
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2020-10-01 09:01:56 +0530

    update loading, add completions

--- plugin.zsh
+++ plugin.zsh
@@ -4,11 +4,12 @@ ACLI=arduino-cli
 err=$(command -v $JQ &> /dev/null || printf "%s" $JQ)
 err+=$(command -v $ACLI &> /dev/null || printf ", %s" $ACLI)
 if [[ -z "$err" ]]; then
-    autoload ar_configure
-    autoload ar_build
-    autoload ar_upload
-    autoload ar_serial
-    autoload ar_env
+    autoload -Uz ar_configure
+    autoload -Uz ar_build
+    autoload -Uz ar_upload
+    autoload -Uz ar_serial
+    autoload -Uz ar_env
+    $ACLI completion zsh > _arduino-cli
     # echo "arduino plugin registered"
 else
     echo "zsh-arduino: Commands not found on path: $err; Plugin not registered"
