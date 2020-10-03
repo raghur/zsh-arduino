538d90753c1a8baed55434e443d1b0fe65de66ee
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2020-10-03 18:44:11 +0530

    fix plugin

--- plugin.zsh
+++ plugin.zsh
@@ -1,16 +1,17 @@
-#! /bin/bash
+#! /bin/zsh
 JQ=jq
 ACLI=arduino-cli
 err=$(command -v $JQ &> /dev/null || printf "%s" $JQ)
 err+=$(command -v $ACLI &> /dev/null || printf ", %s" $ACLI)
 if [[ -z "$err" ]]; then
-    FPATH=$FPATH:"$(0:A)"
+    FPATH=$FPATH:"${0:A:h}"
     autoload -Uz ar_configure
     autoload -Uz ar_build
     autoload -Uz ar_upload
     autoload -Uz ar_serial
     autoload -Uz ar_env
-    $ACLI completion zsh > "$(0:A)/_arduino-cli"
+    [ -r _arduino_cli ] || $ACLI completion zsh > "${0:A:h}/_arduino-cli"
+    source _arduino_cli
     # echo "arduino plugin registered"
 else
     echo "zsh-arduino: Commands not found on path: $err; Plugin not registered"
